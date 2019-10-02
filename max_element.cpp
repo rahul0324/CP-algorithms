4a03012950ce64a986567f8332a124348d822d48
reclone <https://recloneRk@bitbucket.org/sachinitk/upsc.git>
2019-10-02 21:38:37 +0530

    first commit added

---
+++ max_element.cpp
@@ -0,0 +1,24 @@
+// Author : reclone_rk
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main()
+{
+	int n;
+	cout<<"Enter number of elements"<<endl;
+	cin>>n;
+
+	cout<<"Enter elements"<<endl;
+	int a[n];
+
+	int max_element = INT_MIN;
+	for(int i=0; i<n; i++)
+	{
+		cin<<a[i];
+		max_element = max(max_element,a[i]);
+	}
+
+	cout<<max_element<<endl;
+	
+	return 0;
+}
