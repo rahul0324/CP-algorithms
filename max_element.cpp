f99442ae8b09ba814a35c696be4f05d870cc705a
Rishi Sharma <39599919+kampaitees@users.noreply.github.com>
2020-10-01 00:27:23 +0530

    done the workplease push it for hacktoberfest

--- max_element.cpp
+++ max_element.cpp
@@ -22,3 +22,26 @@ int main()
 	
 	return 0;
 }
+
+
+
+int findMaxSum(node* root, int& ans){
+    if(root == NULL)
+        return 0;
+    if(root->left == NULL and root->right == NULL)
+        return root->data;
+    int l = findMaxSum(root->left, ans);
+    int r = findMaxSum(root->right, ans);
+    int val=root->data, maxval=INT_MIN;
+    if(root->left != NULL){
+        maxval = max(maxval, l);
+        val += l;
+    }
+    if(root->right != NULL){
+        maxval = max(maxval, r);
+        val += r;
+    }
+    if(root->left != NULL and root->right != NULL)
+        ans = max(ans, val);
+    return root->data+maxval;
+}
