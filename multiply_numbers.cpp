0b50950df32fd910e42d79e4b8453de79c3adbfe
compiletimeterror <56031091+compiletimeterror@users.noreply.github.com>
2020-10-01 14:41:57 +0530

    added multiply numbes

---
+++ multiply_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int multiplyNumbers(int a, int b){
+    return a*b;
+}
+int main(){
+    int a, b;
+    cin>>a>>b;
+    cout<<multiplyNumbers(a, b)<<endl;
+    return 0;
+}
+
+
